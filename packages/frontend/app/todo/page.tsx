f2a4e99f0070504843c3026ee9f978e35f5d01b4
gilluan formiga <gilluan.formiga@gmail.com>
2023-11-04 01:13:45 +0100

    update todo page

--- packages/frontend/app/todo/page.tsx
+++ packages/frontend/app/todo/page.tsx
@@ -8,7 +8,7 @@ export default function CampaignPage() {
     <>
       <div className="hidden space-y-6 p-10 pb-16 md:block">
         <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Campaign</h2>
+          <h2 className="text-2xl font-bold tracking-tight">Todo</h2>
           <p className="text-muted-foreground">
             Manage your campaign settings.
           </p>
