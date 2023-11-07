d824255ea0320522de3f37181e4b901e06a28dbf
gilluan formiga <gilluan.formiga@gmail.com>
2023-11-07 23:46:39 +0100

    define /todo route as default

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/todo",
+        permanent: true,
+      },
+    ];
+  },
+
   webpack: (config, { webpack, isServer, nextRuntime }) => {
     // Avoid AWS SDK Node.js require issue
     if (isServer && nextRuntime === "nodejs")
